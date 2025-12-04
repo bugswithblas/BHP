ea768fd2fb31a36eefa08a5aeaa57d731d60154a
bugswithblas <blas.gutowski@gmail.com>
2025-12-04 11:28:08 +0100

    TCP Proxy finished

--- udp_client.py
+++ udp_client.py
@@ -5,9 +5,9 @@ target_port = 9997
 
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-client.sendto(b"AAABBBCCC", (target_host, target_port))
+client.sendto(b"hello", (target_host, target_port))
 
 data, addr = client.recvfrom(4096)
 
 print(data.decode())
-client.close
+client.close()
